4b620b05d074c8d1223c725cc70e922d5ebd9804
Anton Mushnin <anton.mushnin@gmail.com>
2022-09-14 13:40:05 +0300

    base currency from locale

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+/* eslint-disable max-len */
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
+import clm from 'country-locale-map';
 import getSymbols from './store/actions/symbols';
 // import logo from './logo.svg';
 import './App.css';
+import SelectCurrency from './components/SelectCurrency';
 
 function App() {
   const dispatch = useDispatch();
@@ -10,20 +13,32 @@ function App() {
   const symbols = useSelector((state) => state.symbols.symbols);
   const loading = useSelector((state) => state.symbols.loading);
   const error = useSelector((state) => state.symbols.error);
-  const [selected, setSelected] = useState('USD');
+  // const [selected, setSelected] = useState('USD');
+  const [from, setFrom] = useState('');
+  const [to, setTo] = useState('EUR');
 
   // const fullNames = useRef();
   // const success = useSelector((state) => state.symbols.success);
 
   useEffect(() => {
+    // console.log(navigator.language.slice(-2));
+    // console.log(Intl.DateTimeFormat().resolvedOptions().locale);
+    setFrom(clm.getCurrencyByAlpha2(navigator.language.slice(-2)));
+    // console.log(clm.getCurrencyByAlpha2(navigator.language.slice(-2)));
     if (!tickers.length) {
       dispatch(getSymbols());
       console.log('fetch');
     }
   }, []);
 
-  const handleSelect = (e) => {
-    setSelected(e.target.value);
+  const handleSelectFrom = (value) => {
+    setFrom(value);
+  };
+  const handleSelectTo = (value) => {
+    setTo(value);
+  };
+  const reverse = () => {
+
   };
 
   return (
@@ -32,22 +47,11 @@ function App() {
       {tickers.length === 0 && !loading && <p>No symbols available!</p>}
       {error && !loading && <p>{error}</p>}
       {tickers.length && (
-        <div>
-          <select value={selected} onChange={handleSelect}>
-            {tickers.map((ticker) => (
-              <option key={ticker} value={ticker}>
-                {symbols[ticker]}
-              </option>
-            ))}
-          </select>
-          <select value={selected} onChange={handleSelect}>
-            {tickers.map((ticker) => (
-              <option key={ticker} value={ticker}>
-                {ticker}
-              </option>
-            ))}
-          </select>
-        </div>
+        <>
+          <SelectCurrency value={from} onChange={handleSelectFrom} tickers={tickers} symbols={symbols} />
+          <SelectCurrency value={to} onChange={handleSelectTo} tickers={tickers} symbols={symbols} />
+          <button type="button" onClick={reverse}>Reverse</button>
+        </>
       )}
 
     </>
---
+++ src/components/SelectCurrency/index.js
@@ -0,0 +1,34 @@
+// import styles from './SelectCurrency.module.css';
+// import { useState } from 'react';
+
+function SelectCurrency({
+  value, onChange, tickers, symbols,
+}) {
+  // const [selected, setSelected] = useState(value);
+
+  const handleSelect = (e) => {
+    // setSelected(e.target.value);
+    onChange(e.target.value);
+  };
+
+  return (
+    <div>
+      <select value={value} onChange={handleSelect}>
+        {tickers.map((ticker) => (
+          <option key={ticker} value={ticker}>
+            {symbols[ticker]}
+          </option>
+        ))}
+      </select>
+      <select value={value} onChange={handleSelect}>
+        {tickers.map((ticker) => (
+          <option key={ticker} value={ticker}>
+            {ticker}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default SelectCurrency;
